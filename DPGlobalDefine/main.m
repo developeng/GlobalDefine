46fec5f69040f868ff4b184f6a4f6ac469e9b454
赵朋 <developeng@developengdepro.local>
2018-01-13 10:13:14 +0800

    Initial Commit

---
+++ DPGlobalDefine/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DPGlobalDefine
+//
+//  Created by developeng on 2018/1/13.
+//  Copyright © 2018年 developeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
