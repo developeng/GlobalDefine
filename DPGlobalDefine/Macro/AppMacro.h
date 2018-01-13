782cb4bd898651cc585c77772ced02796e763201
developeng <developeng@developengdePro.local>
2018-01-13 10:21:49 +0800

    add define

---
+++ DPGlobalDefine/Macro/AppMacro.h
@@ -0,0 +1,21 @@
+//
+//  AppMacro.h
+//  DPGlobalDefine
+//
+//  Created by developeng on 2018/1/13.
+//  Copyright © 2018年 developeng. All rights reserved.
+//
+
+#ifndef AppMacro_h
+#define AppMacro_h
+
+// ----------------------  UIImage  ------------------------/
+#define DefauntImage [UIImage imageNamed:@"goodsPlaceImage"] //商品默认图
+
+// ----------------------  数据处理  ------------------------/
+#define objAssert(obj)  if(!obj)\
+obj = @"";
+
+
+
+#endif /* AppMacro_h */
---
+++ DPGlobalDefine/Macro/UtilsMacro.h
@@ -0,0 +1,21 @@
+//
+//  UtilsMacro.h
+//  DPGlobalDefine
+//
+//  Created by developeng on 2018/1/13.
+//  Copyright © 2018年 developeng. All rights reserved.
+//
+
+#ifndef UtilsMacro_h
+#define UtilsMacro_h
+
+// ----------------------  App 信息获取  ---------------------/
+// 屏幕宽、高、尺寸
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_SIZE [UIScreen mainScreen].bounds.size
+#define KEY_WINDOW [[UIApplication sharedApplication] keyWindow]
+
+
+
+#endif /* UtilsMacro_h */
